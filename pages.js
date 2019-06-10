83a62faa3cf548f7ee539157a9cd34e208dd4989
sefraoui1992 <51467035+sefraoui1992@users.noreply.github.com>
2019-06-10 06:12:08 +0200

    Update pages.js

--- pages.js
+++ pages.js
@@ -2,7 +2,7 @@ var VERIFY_TOKEN = "123456";
 var PAGES = [
 	// Add your pages here, change key for each one, I use numberical values starting at 0
     {
-        id: "2643187955925802",
+        id: "305341463683530",
         name: "test001",
         key: "0",
         title: "It's a brand new day, let's continue our adventure!",
@@ -10,7 +10,7 @@ var PAGES = [
         cta: "Play Now",
         imageurl: "https://www.cmo4hire.com/wp-content/uploads/2016/10/Blog-background-hello-world.jpg",
         payload: null,
-        pat: "EAAP4UambgGQBAM76fiwIjZCIYoJeDgFa5ei4keMRfTZCMfdb1f4sOpCLuFvupl3eCbXWgsHFW6fsbvKFrBvyHlbqKWim0kuWNVkxzcHg8GkbVAEDrQKUcAXqhVVGeAbtOJt0VnVznWx3b2IR0JRkwLmkWTVgJwp6My6npeCzWRJaZCsrfCc",
+        pat: "EAAE1UZA9r8KkBAEkznlgZBufHaNRyzlWKR0NbfzxjSQ8JwD8EJg5D9ZCg7zKYnMMAGzEev7P2obE4P51m5UbhJ7t9CErtQVJbKnzXkJ2j7BpxedXZC2q4ItDnfJuNeKtJjIX8F3sTTuLEqZAXcwdPQY720rQgEZCMi6XDkPVEqZCuAOgbsZB3twmLyrL5ZCiW32QZD",
     },
 	
 
