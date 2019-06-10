9836623d39c1b476f99ae38842d475455f612358
sefraoui1992 <51467035+sefraoui1992@users.noreply.github.com>
2019-06-10 05:50:56 +0200

    Update pages.js

--- pages.js
+++ pages.js
@@ -2,7 +2,7 @@ var VERIFY_TOKEN = "123456";
 var PAGES = [
 	// Add your pages here, change key for each one, I use numberical values starting at 0
     {
-        id: "619915325180106",
+        id: "2643187955925802",
         name: "test001",
         key: "0",
         title: "It's a brand new day, let's continue our adventure!",
