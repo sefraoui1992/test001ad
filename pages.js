4cdbb5eb86a1539db0ed0ff017b9b69b01532658
sefraoui1992 <51467035+sefraoui1992@users.noreply.github.com>
2019-06-10 04:40:09 +0200

    Update pages.js

--- pages.js
+++ pages.js
@@ -1,16 +1,16 @@
-var VERIFY_TOKEN = "mygame";
+var VERIFY_TOKEN = "123456";
 var PAGES = [
 	// Add your pages here, change key for each one, I use numberical values starting at 0
     {
-        id: "833719527002847",
-        name: "Med test1",
+        id: "619915325180106",
+        name: "test001",
         key: "0",
         title: "It's a brand new day, let's continue our adventure!",
         subtitle: "Your Friends are playing...",
         cta: "Play Now",
         imageurl: "https://www.cmo4hire.com/wp-content/uploads/2016/10/Blog-background-hello-world.jpg",
         payload: null,
-        pat: "EAAIkfPKGV3EBAB5ZCuguCWMS98V9WEGd7UC82WH3OfhI3c3ZA13PLzF5TYZB587dZCUajAr3jsrDxwR9QUGkyngsMicZBnMFZBBqx1FIko9fsqJ7JbD0ygBAYdqiBiF3hMpl1wma9tuV46wsOHqWu739KoKfZCkXY1LHKqlzEQ5DAcZCNiRnD146",
+        pat: "EAAP4UambgGQBAEdVGDTKyZCd5xpHje4ZAVX9OlyXMZB9ZCqPAolOzlLfZA84yfo4RThc1ShOYFKxQkuXpZB7oLQBCNBZCE0W1vXWjNpMWhZAh5D0YNF9iHuxHJv9qZCtAtTacayPtuI08ZCzQ3UJoS2ddIbL9ZByXH3odXMAiRQvGkyeIlOc3JIr3GN",
     },
 	
 
